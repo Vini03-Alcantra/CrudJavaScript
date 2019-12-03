a0591933bc8edd8f1aad4fc41d9065443cf469b5
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2019-12-02 21:34:41 -0300

    Primeira versão de um Crud sendo aplicado em um sistema de cadastro e update

---
+++ js/config.js
@@ -0,0 +1,10 @@
+function setConfig(){
+    var texts = {
+        "title":"Shopping So will I"
+    };
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+
+}
+
+setConfig();
